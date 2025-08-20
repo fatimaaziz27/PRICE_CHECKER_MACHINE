eb51841097b17e48cea393ea25afb2da5c0eb1d9
fatimaaziz27 <fatimaaziz250907@gmail.com>
2025-08-20 23:07:55 +0500

    Add files via upload

---
+++ price_checking_machine.py
@@ -0,0 +1,21 @@
+#A machine that takes user input for product name and tells the price of the product if it's available in the list
+
+products = {
+    "Apple": 100,
+    "Banana": 50,
+    "Orange": 75,
+    "Mango": 200,
+    "Grapes": 150
+}
+
+def get_product_price(product_name):
+    if product_name in products:
+        return f"The price of {product_name} is {products[product_name]} PKR"
+    else:
+        return f"Sorry, {product_name} is not available or not for sale."
+
+while True:
+    product_name = input("Enter product name (or 'quit' to stop): ")
+    if product_name.lower() == 'quit':
+        break
+    print(get_product_price(product_name.capitalize()))
